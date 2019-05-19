213d617354446032c58b9296c5ad93304a0e02b2
MaxiAybar <50594928+MaxiAybar@users.noreply.github.com>
2019-05-19 17:11:57 -0300

    Add files via upload

---
+++ empleado.cpp
@@ -0,0 +1,8 @@
+#inlcude "empleado.h"
+#inlcude "trabajador.h"
+
+void Empleado::calcularSueldo(){
+	if(ausencias == 0){
+		if(llegadas_tarde
+	}
+}
--- empresa.cpp
+++ empresa.cpp
@@ -1,5 +1,6 @@
 #include "empresa.h"
 #include "trabajador.h"
+#include "empleado.h"
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -11,5 +12,49 @@ Empresa::Empresa(){
 
 void Empresa::cargarTrabajadores(){
 	ifstream txt("trabajadores.txt");
+	Trabajador* aux;
+	string nombre;
+	int legajo;
 	
+	while(!txt.eof()){
+		char tipo;
+		txt>> tipo;
+		if(tipo == 'E'){
+			aux = new Empleado;
+			int sueldo_mensual;
+			int llegadas_tarde;
+			int ausencias;
+			txt>> legajo>> nombre>> sueldo_mensual>> llegadas_tarde>> ausencias;
+			aux.asignarSueldoMensual(sueldo_mensual);
+			aux.asignarLlegadasTarde(llegadas_tarde);
+			aux.asignarAusencias(ausencias);
+			aux.asignarTrabajador(tipo, nombre, legajo, aux.calcularSueldo());
+			trabajadores.push_back(aux);
+			delete aux;
+		}
+		else if(tipo == 'J'){
+			aux = new Jornalero;
+			int valor_diario;
+			int cant_dias;
+			txt>> legajo>> nombre>> valor_diario>> cant_dias;
+			aux.asignarValorDiario(valor_diario);
+			aux.asignarCantDias(cant_dias);
+			aux.asignarTrabajador(tipo, nombre, legajo, aux.calcularSueldo());
+			trabajadores.push_back(aux);
+			delte aux;
+		}
+		else if(tipo == 'C'){
+			aux = new Consultor;
+			int valor_hora_catedra;
+			int horas_catedras;
+			int horas_a_descontar;
+			txt>> legajo>> nombre>> valor_hora_catedra>> horas_catedras>> horas_a_descontar;
+			aux.asignarValorHora(valor_hora_catedra);
+			aux.asignarHorasCatedras(horas_catedras);
+			aux.asignarHorasADescontar(horas_a_descontar);
+			aux.asignarTrabjador(tipo, nombre, legajo, aux.calcularSueldo());
+			delete aux;
+		}
+	}
+	txt.close();
 }
